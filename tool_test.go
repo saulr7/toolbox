12876ee01669a65ef9746cf35f9e576181c43f2f
Saul Ramirez <fernando.ramire.hn@gmail.com>
2023-07-14 16:46:54 -0600

    ✅ adding test case

--- tool_test.go
+++ tool_test.go
@@ -165,3 +165,21 @@ func TestTools_UploadOneFile(t *testing.T) {
 	_ = os.Remove(fmt.Sprintf("./testdata/upload/%s", uploadedFile.NewFileName))
 
 }
+func TestTools_CreateDirIfNotExists(t *testing.T) {
+
+	var testTool Tools
+
+	err := testTool.CreateDirIfNotExist("./testdata/myDir")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = testTool.CreateDirIfNotExist("./testdata/myDir")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	os.Remove("./testdata/myDir")
+}
